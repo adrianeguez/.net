195091815e3f86cc92de295b87b4249ae8691101
Vicente Adrian Eguez Sarzosa <eadepto@hotmail.com>
2023-08-31 18:12:35 -0500

    feat: anadir proyecto .new core API y controller Author

---
+++ 03/PubAPI/PublisherData/PubContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PublisherDomain;
+
+namespace PublisherData
+{
+    public class PubContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Cover> Cover{ get; set; }
+        public DbSet<Artist> Artist { get; set; }
+        public DbSet<AuthorByArtist> AuthorsByArtist { get; set; }
+        public PubContext(DbContextOptions<PubContext> options):base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // ya no necesario porque en el constructor recibimos las opciones
+            //optionsBuilder.UseSqlServer(
+            //    "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase4"
+            //    ).LogTo(
+            //    Console.WriteLine,
+            //    new[] {
+            //        DbLoggerCategory.Database.Command.Name
+            //    },
+            //    LogLevel.Information
+            //    )
+            //    .EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AuthorByArtist>()
+                .HasNoKey() // No tiene PK
+                .ToView(nameof(AuthorsByArtist)); // Mapeado a vista
+        }
+    }
+}
---
+++ 03/PubAPI/PublisherData/obj/Debug/net6.0/3o4f5r2a.sql
@@ -0,0 +1,21 @@
+﻿BEGIN TRANSACTION;
+GO
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'FirstName', N'LastName') AND [object_id] = OBJECT_ID(N'[Authors]'))
+    SET IDENTITY_INSERT [Authors] ON;
+INSERT INTO [Authors] ([Id], [FirstName], [LastName])
+VALUES (1, N'Adrian', N'Eguez'),
+(2, N'Vicente', N'Eguez'),
+(3, N'Carolina', N'Eguez'),
+(4, N'Adrian', N'Sarzosa');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'FirstName', N'LastName') AND [object_id] = OBJECT_ID(N'[Authors]'))
+    SET IDENTITY_INSERT [Authors] OFF;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230831164225_seed-authors', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
